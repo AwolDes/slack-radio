aa9c16dbe125ec0d09e784a00017a61cbbdd3bd7
Austin <awoldes@gmail.com>
2015-11-17 01:54:25 +1000

    [WIP] Implementing Firebase to add songs

---
+++ frontend/app/models/playlist/songs.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    videoId: DS.attr('string'),
+    title: DS.attr('string'),
+    artist: DS.attr('string')
+});
+
+
--- frontend/app/routes/playlist/index.js
+++ frontend/app/routes/playlist/index.js
@@ -1,20 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    
-    
-    
-    /*model() {
-        //return this.store.findAll('friend');
-        
-        return true;
-    }*/
-    
-    actions: {
-        play: function(link) {
-                console.log(link);
-            }
-        }
-    
-    
 });
+    
+    
+    
+    
