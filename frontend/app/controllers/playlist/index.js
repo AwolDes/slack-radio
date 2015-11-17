d3b0c2973047ab2d57e2c4fb22ee727d6c89cc2a
Austin <awoldes@gmail.com>
2015-11-17 18:07:24 +1000

    YouTube URL Regex

--- frontend/app/controllers/playlist/index.js
+++ frontend/app/controllers/playlist/index.js
@@ -4,15 +4,35 @@ export default Ember.Controller.extend({
      
     actions: {
         save: function(link, name, singer) {
-            //console.log(link);
-            var addSong = this.store.createRecord('playlist/song', {
-                videoId:link,
+            console.log(link);
+            //var regex = "/^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/g";
+            
+            // For testing.
+        var urls = [
+            link
+        ];
+
+        var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+        var i = 0;
+        for (i = 0; i < urls.length; ++i) {
+            var r = urls[i].match(rx);
+            console.log(r[1]);
+            var newLink = r[1];
+        }
+            //var newLink = link.search(REGEX);
+            //console.log(newLink);
+            
+            var addSong = this.store.createRecord('playlist/songs', {
+                videoId:newLink,
                 title:name,
                 artist:singer
             });
             addSong.save();
+           
             
-            
+            this.set('videoId', '');
+            this.set('title', '');
+            this.set('artist', '');
             
             return false;
         }
