f89d66d62c81b019d42d9b73b7accc6497bd4427
Austin <awoldes@gmail.com>
2015-11-18 11:49:20 +1000

    [WIP] Playing consecutive songs

--- frontend/app/controllers/playlist/index.js
+++ frontend/app/controllers/playlist/index.js
@@ -1,26 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-     
+   /* playlist:this.get('model'),
+    songs:playlist.get('songs'),
+    youtubeId:songs.get([0]),*/
     actions: {
         save: function(link, name, singer) {
             console.log(link);
-            //var regex = "/^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/g";
-            
             // For testing.
-        var urls = [
-            link
-        ];
+            var urls = [
+                link
+            ];
 
-        var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
-        var i = 0;
-        for (i = 0; i < urls.length; ++i) {
-            var r = urls[i].match(rx);
-            console.log(r[1]);
-            var newLink = r[1];
-        }
-            //var newLink = link.search(REGEX);
-            //console.log(newLink);
+            var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+            var i = 0;
+            for (i = 0; i < urls.length; ++i) {
+                var r = urls[i].match(rx);
+                console.log(r[1]);
+                var newLink = r[1];
+            }
+            
             
             var addSong = this.store.createRecord('playlist/songs', {
                 videoId:newLink,
@@ -33,6 +32,21 @@ export default Ember.Controller.extend({
             this.set('videoId', '');
             this.set('title', '');
             this.set('artist', '');
+            
+            return false;
+        },
+        
+        ytPlaying: function() {
+            Ember.debug('on playing from controller');
+            
+            return false;
+        },
+        
+        ytEnded: function() {
+            Ember.debug('on ended from controller');
+            //Ember.debug(ytid);
+            // here you could load another video by changing the youTubeId
+            //this.set('youtubeId', 'dQw4w9WgXcQ');
             
             return false;
         }
