eac6c8d6455c71b854fe8d150cbd19c6358094ae
austin <awoldes@gmail.com>
2015-12-02 10:28:49 +1000

    Playlist plays all songs

--- frontend/app/controllers/playlist/index.js
+++ frontend/app/controllers/playlist/index.js
@@ -2,10 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    firstLink:Ember.computed.alias('model.firstObject.videoId'),
-    
+
     // Make this equal firstLink
-    link:"",
+    newLink:Ember.computed.alias('model.firstObject.videoId'),
+    currentSong:Ember.computed.alias('model.firstObject.title'),
+    currentArtist:Ember.computed.alias('model.firstObject.artist'),
+
+    songPos:0,
+    
+    songIds: function(){
+       var arr = [];
+       this.get('model').forEach(function(item){
+         var id=item.get('videoId')//.forEach(function(id){
+             //console.log(id)
+           arr.pushObject(id); //some object that is represents each specific combination
+         });
+       //});
+        //console.log(arr)
+        return arr;
+     }.property('model'),
+    
+    //newLink:this.get('someArray'),
+    
+    //listId:this.get('someArray'),
 
     actions: {
         save: function(link, name, singer) {
@@ -41,25 +60,31 @@ export default Ember.Controller.extend({
         
         ytPlaying: function() {
             Ember.debug('on playing from controller');
-            
+            //this.get('someArray');
             //Ember.debug(data);
             return false;
         },
         
         ytEnded: function() {
             Ember.debug('on ended from controller');
-            //Ember.debug(ytid);
+            var current = this.get('newLink');
+            console.log("Current: " + current);
             
-            var nextId = "";
-            this.set('link', nextId)
+            var next = this.incrementProperty('songPos', 1);
+            var nextId = this.get('songIds').objectAt(next);
+            
+            console.log("Next: " + nextId);
+            this.set('newLink', nextId)
             
             return false;
         },
-        
+        // Click on song, plays it.
         getId:function(id, title, singer){
             console.log(id);
-            this.set('link','')
-            this.set('link',id)
+            //this.set('newLink','')
+            this.set('songPos', pos)
+            this.set('newLink',id)
         }
     }
 });
+
