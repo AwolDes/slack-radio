bc23b9ad5d7d26f7b157ef43bfb6976d9bf422a0
austin <awoldes@gmail.com>
2016-01-22 18:55:46 +1000

    Commented code

--- frontend/app/controllers/playlist.js
+++ frontend/app/controllers/playlist.js
@@ -2,37 +2,34 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    // Make this equal firstLink
+//Grab the first video from the array in forebase
     newLink:Ember.computed.alias('model.firstObject.videoId'),
     currentSong:Ember.computed.alias('model.firstObject.title'),
     currentArtist:Ember.computed.alias('model.firstObject.artist'),
-
+// Declare sonPos variable
     songPos:0,
-    
+    // Create an array of all video IDs to use later
     songIds: function(){
        var arr = [];
        this.get('model').forEach(function(item){
-         var id=item.get('videoId')//.forEach(function(id){
-             //console.log(id)
-           arr.pushObject(id); //some object that is represents each specific combination
+         var id=item.get('videoId')
+           arr.pushObject(id);
          });
-       //});
         //console.log(arr)
         return arr;
      }.property('model'),
     
-    //newLink:this.get('someArray'),
-    
-    //listId:this.get('someArray'),
-
+    // What happens when buttons are clicked etc.
     actions: {
+        // Take parsmeters, seen in playlist.hbs
         save: function(link, name, singer) {
             console.log(link);
             // For testing.
             var urls = [
                 link
             ];
-
+            // Magic, never touch.
+            // Regular expression for finding the youtube video ID from a youtube URL
             var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
             var i = 0;
             for (i = 0; i < urls.length; ++i) {
@@ -41,24 +38,25 @@ export default Ember.Controller.extend({
                 var newLink = r[1];
             }
             
-            /*var playlist = this.store.peekRecord('room', 1);
-            console.log(playlist)*/
+            // Add a new song, notice how iit is formed from the playlist model.
             var addSong = this.store.createRecord('song', {
                 videoId:newLink,
                 title:name,
                 artist:singer,
                 //room:playlist
             });
+            // SAVE it to firebase
             addSong.save();
            
-            
+            // Reset form values in playlist.hbs
             this.set('videoId', '');
             this.set('title', '');
             this.set('artist', '');
             
             return false;
         },
-        
+        // More magic
+        // Used with an ember component that loads the youtube video.
         ytPlaying: function() {
             Ember.debug('on playing from controller');
             //this.get('someArray');
